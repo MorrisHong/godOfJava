a61c335649cb30641871fd0b255f3e92483b6a0e
gracelove <gracelove@DESKTOP-1I1P91F>
2019-04-24 01:50:30 +0900

    tripleOrAnd method init

--- src/ch6/ControlIfAndOr.java
+++ src/ch6/ControlIfAndOr.java
@@ -18,4 +18,13 @@ public class ControlIfAndOr {
 			System.out.println("Age is over 25 or Married");
 		}
 	}
+	
+	public void tripleOrAnd() {
+		int age = 25;
+		boolean isMarried = true;
+		double height = 180;
+		if( (age > 25 || isMarried) && height >= 180) {
+			System.out.println("Age is over 25 or Married and height is over 180");
+		}
+	}
 }
