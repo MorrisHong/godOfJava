488fa807a1469d4c80456032c23b6b242ce1c491
gracelove <gracelove@DESKTOP-1I1P91F>
2019-04-24 12:53:48 +0900

    passByValue.

---
+++ src/ch8/ReferencePass.java
@@ -0,0 +1,24 @@
+package ch8;
+
+public class ReferencePass {
+	public static void main(String[] args) {
+		ReferencePass reference = new ReferencePass();
+	
+		reference.callPassByValue();
+	}
+	public void callPassByValue() {
+		int a = 10;
+		String b = "b";
+		System.out.println("before passByValue");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		passByValue(a,b);
+	}
+	public void passByValue(int a, String b) {
+		a = 20;
+		b = "z";
+		System.out.println("in passByValue");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+}
