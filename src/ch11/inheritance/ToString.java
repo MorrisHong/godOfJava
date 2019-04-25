3cbaad41e8549a58ae776e2162e75c3f89fb8fbe
gracelove <gracelove@DESKTOP-1I1P91F>
2019-04-25 19:35:56 +0900

    override toString

---
+++ src/ch11/inheritance/ToString.java
@@ -0,0 +1,19 @@
+package ch11.inheritance;
+
+public class ToString {
+	public static void main(String[] args) {
+		ToString thisObject = new ToString();
+		thisObject.toStringMethod();
+	}
+	
+	public void toStringMethod() {
+		System.out.println(this);
+		System.out.println(this.toString());
+		System.out.println("plus" +this);
+	}
+	
+	@Override
+	public String toString() {
+		return "ToString.toString()";
+	}
+}
